a0fe5031d5e6db3c34bbf260bd8eb9074f91bd6b
YoussufShakweh <yousef.shakwa@gmail.com>
2023-09-19 23:07:42 +0300

    Modified

--- main.py
+++ main.py
@@ -3,31 +3,37 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import WebDriverException
 
-# Enter The Student Number
-std_num = input("Enter Your Number: ")
 
-url = "http://moed.gov.sy/9th/index.php"
-driver = webdriver.Edge()
+def main() -> None:
+    # Enter The Student Number
+    std_num = input("Enter Your Number: ")
 
-while True:
-  try: 
-    driver.get(url)
-    break
-  except WebDriverException:
-    driver.refresh()
+    url = "http://moed.gov.sy/9th/index.php"
+    driver = webdriver.Edge()
 
-# Find Selection Webelement "Branch" and Select Basic Branch
-Select(driver.find_element(By.ID, "branch")).select_by_value("1")
+    while True:
+        try:
+            driver.get(url)
+            break
+        except WebDriverException:
+            driver.refresh()
 
-# Find Selection Webelement "City" and Select Hama
-Select(driver.find_element(By.ID, "city")).select_by_value("7")
+    # Find Selection Webelement "Branch" and Select Basic Branch
+    Select(driver.find_element(By.ID, "branch")).select_by_value("1")
 
-# Find Webelement "stdnum" and Fill It
-driver.find_element(By.ID, "stdnum").send_keys(std_num)
+    # Find Selection Webelement "City" and Select Hama
+    Select(driver.find_element(By.ID, "city")).select_by_value("7")
 
-# Find Webelement "Submit" and Click It
-driver.find_element(By.ID, "submit").click()
+    # Find Webelement "stdnum" and Fill It
+    driver.find_element(By.ID, "stdnum").send_keys(std_num)
 
-input("Press to close ...")
+    # Find Webelement "Submit" and Click It
+    driver.find_element(By.ID, "submit").click()
 
-driver.quit()
+    input("Press to close ...")
+
+    driver.quit()
+
+
+if __name__ == '__main__':
+    main()
